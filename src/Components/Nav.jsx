39e4b48c944ca1412cefc618f81f680c58e9bd6d
Pattou Emad <pattouemad@Partinias-MacBook.local>
2026-03-29 22:40:31 +0200

    Initial React project

---
+++ src/Components/Nav.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Nav.css'
+import logo from '../'
+const Nav = () => {
+    return (  <>
+    
+    <div className='nav'>
+        <img src="" alt="" />
+        
+    </div>
+    
+    
+    </>);
+}
+ 
+export default Nav;
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './Home.css'
+
+const Home = () => {
+    return ( <>
+    
+    
+    
+    
+    </> );
+}
+ 
+export default Home;
