c1de0fe883750e2807ac2f7bd4a6f4b0c9ec5bf4
Pattou Emad <pattouemad@Partinias-MacBook.local>
2026-04-01 13:03:00 +0200

    created blog detail page and added nav nar and title

--- src/Pages/Blogdetail.jsx
+++ src/Pages/Blogdetail.jsx
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
 import Nav from '../Components/Nav';
-
-
+import MainTitle from '../Components/MainTitle';
+import '../Pages/Blogdetail.css'
+import blog2 from '../Assets/Img/blog2.png'
 
 const Blogdetail = () => {
     return (  <>
     <Nav />
     
+    <MainTitle 
+    t1='CHARGING INSIGHTS'
+    />
     
-    
+  <div class="item large">
+      <Link to="/Blogdetail">
+      <img src={blog2} alt="EV Car" />
+      </Link>
+      <span class="date">Sep 06, 2022</span>
+    </div>
+
     </>);
 }
  
