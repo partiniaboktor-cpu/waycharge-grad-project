852dca72d1a8ad7f0a91fa965129b38e19ea9db3
Pattou Emad <pattouemad@Partinias-MacBook.local>
2026-04-04 00:01:03 +0200

    Nav bar responsive with burger menu

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,46 +1,38 @@
 import React, { useEffect, useState } from "react";
-import './Nav.css'
-import wholoelogo from '../Assets/Img/wholologo.svg'
-import language from '../Assets/Icons/language.svg'
+import './Nav.css';
+import wholoelogo from '../Assets/Img/wholologo.svg';
+import language from '../Assets/Icons/language.svg';
 import { Link } from "react-router-dom";
 import { supabase } from "../Supabase";
 
 const Nav = () => {
+  const [NavData, setNavData] = useState([]);
+  const [isOpen, setIsOpen] = useState(false); // burger menu state
 
-const [Nav, setNav] = useState([]);
-useEffect(() => {
-
-  async function getNavAPI() {
-    const { data, error } = await supabase
-      .from("Home")
-      .select("*");
-
-    if (error) {
-      console.log(error);
-    } else {
-      setNav(data);
-      console.log(data);
+  useEffect(() => {
+    async function getNavAPI() {
+      const { data, error } = await supabase.from("Home").select("*");
+      if (error) console.log(error);
+      else setNavData(data);
     }
-  }
+    getNavAPI();
+  }, []);
 
-  getNavAPI();
+  return (
+    <nav className='nav-container'>
+      <Link to="/">
+        <img className='logo' src={wholoelogo} alt="logo" />
+      </Link>
 
-}, []);
+      {/* Burger icon */}
+      <div className={`burger ${isOpen ? "open" : ""}`} onClick={() => setIsOpen(!isOpen)}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
 
-    return (  <>
-    
-    <div className='nav'>
-<Link to="/">
-{/* {
-Nav
-.filter(Nav => Nav.id === 1)
-.map(Nav => (
-  <h2 key={Nav.id} > {Nav.image}</h2>
-))
-} */}
-<img className='logo' src={wholoelogo} alt="" />
-</Link>
-    <div className='menu'>
+      {/* Menu links */}
+      <div className={`menu ${isOpen ? "menu-open" : ""}`}>
         <Link to="/about" className="menu1">About us</Link>
         <Link to="/mobileapp" className='menus2'>Mobile app</Link>
         <Link to="/stations" className='menus3'>Stations</Link>
@@ -49,22 +41,10 @@ Nav
         <Link to="/Contactus"className='menus5'>Contact us</Link>
         <Link to="/Careers" className='menus7'>Careers</Link>
         <Link to="/help" className='menus6'>Help</Link>
-    </div>
-        
+        <img className='language' src={language} alt="language" />
+      </div>
+    </nav>
+  );
+};
 
-{/* {
-Nav
-.filter(Nav => Nav.id === 2)
-.map(Nav => (
-  <h2 key={Nav.id} className='language'> {Nav.image}</h2>
-))
-} */}
-<img className='language' src={language} alt="" />
-
-    </div>
-    
-    
-    </>);
-}
- 
 export default Nav;
