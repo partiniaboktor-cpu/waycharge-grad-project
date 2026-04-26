7e77ad3c74f5598e513088b1fd1d01130cbdf5e9
Pattou Emad <pattouemad@Partinias-MacBook.local>
2026-04-26 16:38:38 +0300

    edits in vlog

--- src/Common/Blogpart.jsx
+++ src/Common/Blogpart.jsx
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import  './Blogpart.css'
+import React, { useRef } from 'react';
+import './Blogpart.css'
 import blog1 from '../Assets/Img/blog1.png'
 import blog2 from '../Assets/Img/blog2.png'
 import blog3 from '../Assets/Img/blog3.png'
@@ -7,57 +7,87 @@ import blog4 from '../Assets/Img/blog4.png'
 import blog5 from '../Assets/Img/blog5.png'
 import blog6 from '../Assets/Img/blog6.png'
 import { Link } from "react-router-dom";
+import { motion, useScroll, useTransform } from "framer-motion";
 
-const blog = () => {
-    return ( <>
-    
-<div class="gallery">
-    <div class="item large">
-        <Link to="/Blogdetail">
-      <img src={blog1} alt="EV Car" />
-      </Link>
-      <span class="date">Sep 06, 2022</span>
-    </div>
+const Blogpart = () => {
+    const targetRef = useRef(null);
+    const { scrollYProgress } = useScroll({
+      target: targetRef,
+    });
 
-    <div class="item large">
-      <Link to="/Blogdetail">
-      <img src={blog2} alt="EV Car" />
-      </Link>
-      <span class="date">Sep 06, 2022</span>
-    </div>
+    // Move the container to the left by up to 75% of its width
+    const x = useTransform(scrollYProgress, [0, 1], ["0%", "-75%"]);
 
-    <div class="item small">
-       <Link to="/Blogdetail">
-      <img src={blog3} alt="EV Car" />
-      </Link>
-      <span class="date">Sep 06, 2022</span>
-    </div>
+    return ( 
+      <section ref={targetRef} className="blog-scroll-section">
+        <div className="blog-sticky-container">
+          <motion.div style={{ x }} className="gallery">
+            <div className="item">
+                <Link to="/Blogdetail">
+              <img src={blog1} alt="EV Car" />
+              </Link>
+              <span className="date">Sep 06, 2022</span>
+            </div>
 
-    <div class="item small">
-      <Link to="/Blogdetail">
-      <img src={blog4} alt="EV Car" />
-      </Link>
-      <span class="date">Sep 06, 2022</span>
-    </div>
+            <div className="item">
+              <Link to="/Blogdetail">
+              <img src={blog2} alt="EV Car" />
+              </Link>
+              <span className="date">Sep 06, 2022</span>
+            </div>
 
- <div class="item small">
-  <Link to="/Blogdetail">
-      <img src={blog5} alt="EV Car" />
-      </Link>
-      <span class="date">Sep 06, 2022</span>
-    </div>
+            <div className="item">
+               <Link to="/Blogdetail">
+              <img src={blog3} alt="EV Car" />
+              </Link>
+              <span className="date">Sep 06, 2022</span>
+            </div>
 
+            <div className="item">
+              <Link to="/Blogdetail">
+              <img src={blog4} alt="EV Car" />
+              </Link>
+              <span className="date">Sep 06, 2022</span>
+            </div>
 
-     <div class="item small">
-      <Link to="/Blogdetail">
-      <img src={blog6} alt="EV Car" />
-      </Link>
-      <span class="date">Sep 06, 2022</span>
-    </div>
+            <div className="item">
+              <Link to="/Blogdetail">
+                  <img src={blog5} alt="EV Car" />
+              </Link>
+              <span className="date">Sep 06, 2022</span>
+            </div>
 
-  </div>
-    
-    </> );
+            <div className="item">
+              <Link to="/Blogdetail">
+              <img src={blog6} alt="EV Car" />
+              </Link>
+              <span className="date">Sep 06, 2022</span>
+            </div>
+
+            <div className="item">
+              <Link to="/Blogdetail">
+              <img src={blog1} alt="EV Car" />
+              </Link>
+              <span className="date">Sep 07, 2022</span>
+            </div>
+
+            <div className="item">
+              <Link to="/Blogdetail">
+              <img src={blog2} alt="EV Car" />
+              </Link>
+              <span className="date">Sep 08, 2022</span>
+            </div>
+
+            <div className="item">
+              <Link to="/Blogdetail">
+              <img src={blog3} alt="EV Car" />
+              </Link>
+              <span className="date">Sep 09, 2022</span>
+            </div>
+          </motion.div>
+        </div>
+      </section>
+    );
 }
  
-export default blog;
+export default Blogpart;
